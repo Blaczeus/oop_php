64341e089b65bd5f29c18d0a5f5fdfd68b8b7c20
Anya Chidiebere <anyachidiebere029@gmail.com>
2023-08-28 17:06:15 +0100

    Multiple requests

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/oop_php',
+        'pretty_version' => 'dev-dev-01',
+        'version' => 'dev-dev-01',
+        'reference' => '43d4335e1488cd9901727dfaf8baf95079295c6c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/oop_php' => array(
+            'pretty_version' => 'dev-dev-01',
+            'version' => 'dev-dev-01',
+            'reference' => '43d4335e1488cd9901727dfaf8baf95079295c6c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
