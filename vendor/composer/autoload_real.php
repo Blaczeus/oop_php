5285ae196ccaadc018b49d1a7e1f51429281240d
Anya Chidiebere <anyachidiebere029@gmail.com>
2023-11-10 16:34:06 +0100

    Installed php pest package and Illuminate/Collection with composer and implemented the autoload file

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitfdac7664d2c5d0d548757b65c50dbae7
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitfdac7664d2c5d0d548757b65c50dbae7', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitfdac7664d2c5d0d548757b65c50dbae7', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitfdac7664d2c5d0d548757b65c50dbae7
         call_user_func(\Composer\Autoload\ComposerStaticInitfdac7664d2c5d0d548757b65c50dbae7::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitfdac7664d2c5d0d548757b65c50dbae7::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
